c1efe9b115b8e93fd40d71b89a697c5e85a8174c
mustafa.alkan <mustafa.alkan@well.co>
2023-03-27 17:16:58 +0300

    Removed unnecessary codes.

--- app/src/main/java/com/mialkan/aflaprogressdialog/FirstFragment.kt
+++ app/src/main/java/com/mialkan/aflaprogressdialog/FirstFragment.kt
@@ -4,18 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.semantics.heading
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
@@ -41,72 +37,12 @@ class FirstFragment : Fragment() {
 
 @Composable
 fun FirstScreen(navController: NavController) {
-    AFLAProgressDialogTheme {
-        Scaffold(
-            topBar = {
-                TopAppBar(title = {
-                    Text(
-                        text = "Focus Progress",
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                        modifier = Modifier.semantics { heading() }
-                    )
-                })
-            }
-
-        ) { paddingValues ->
-            Box(modifier = Modifier.padding(paddingValues)) {
-                Box(modifier = Modifier.padding(20.dp)) {
-                    Button(
-                        onClick = { navController.navigate(R.id.action_FirstFragment_to_SecondFragment) },
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text(text = "Next Screen")
-                    }
-                }
-            }
+    Box(modifier = Modifier.fillMaxSize().padding(20.dp)) {
+        Button(
+            onClick = { navController.navigate(R.id.action_FirstFragment_to_SecondFragment) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Next Screen")
         }
     }
-}
-
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
-
-@Composable
-fun AFLAProgressDialogTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
-    MaterialTheme(
-        colors = colors,
-        content = content
-    )
 }
--- app/src/main/java/com/mialkan/aflaprogressdialog/SecondFragment.kt
+++ app/src/main/java/com/mialkan/aflaprogressdialog/SecondFragment.kt
@@ -8,18 +8,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.semantics.*
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
 import kotlinx.coroutines.delay
 
 /**
@@ -34,68 +28,42 @@ class SecondFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                BackButtonScreen(findNavController())
+                BackButtonScreen()
             }
         }
     }
 }
 
 @Composable
-fun BackButtonScreen(navController: NavController) {
+fun BackButtonScreen() {
     var showLoading by remember { mutableStateOf(true) }
 
     LaunchedEffect(key1 = showLoading) {
         if (showLoading) {
-            delay(1500)
+            delay(2500)
             showLoading = false
         }
     }
-    AFLAProgressDialogTheme {
-        Scaffold(
-            topBar = {
-                TopAppBar(title = {
-                    Text(
-                        text = "Focus Progress",
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                        modifier = Modifier.semantics { heading() }
-                    )
-                }, navigationIcon = {
-                        IconButton(
-                            onClick = { navController.popBackStack() }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Filled.ArrowBack,
-                                contentDescription = "Navigate Up"
-                            )
-                        }
-                    })
-            }
-        ) { paddingValues ->
-            if (showLoading) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .wrapContentSize(Alignment.Center)
-                ) {
-                    CircularProgressIndicator(color = MaterialTheme.colors.primary, modifier = Modifier.semantics { liveRegion = LiveRegionMode.Polite })
-                }
-            } else {
-                Box(
-                    modifier = Modifier.padding(paddingValues)
-                ) {
-                    Column(
-                        modifier = Modifier.fillMaxWidth()
-                            .verticalScroll(rememberScrollState())
-                    ) {
-                        Spacer(modifier = Modifier.height(20.dp))
-                        Text(
-                            modifier = Modifier.padding(start = 20.dp, end = 20.dp),
-                            text = "How long did you sleep?",
-                            style = MaterialTheme.typography.h6
-                        )
-                    }
-                }
+    if (showLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+        ) {
+            CircularProgressIndicator(color = MaterialTheme.colors.primary)
+        }
+    } else {
+        Box {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    modifier = Modifier.padding(start = 20.dp, end = 20.dp),
+                    text = "How long did you sleep?",
+                    style = MaterialTheme.typography.h6
+                )
             }
         }
     }
